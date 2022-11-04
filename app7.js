9041ee11a6f7c36c8ca887bdbdf6ae164d916926
麻季 <104350064+komuro-058@users.noreply.github.com>
2022-11-04 08:01:20 +0000

    app7.js等を追加

---
+++ app7.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test3.db');
+
+app.set('view engine', 'ejs');
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", (req, res) => {
+  const message = "ジャニの表示をするページです";
+  res.render('show2', {mes:message});
+});
+
+app.get("/team", (req, res) => {
+    db.serialize( () => {
+        db.all("select team_id, team, funs, debut from team;", (error, row) => {
+            if( error ) {
+                res.render('show2', {mes:"エラーです"});
+            }
+            res.render('select2', {data:row});
+        })
+    })
+})
+
+app.get("/team/:team_id", (req,res) => {
+  db.serialized( () => {
+    db.all("select id, name from jidol inner join team on jidol.team_id=team.team_id order by name;")
+  })
+})
+
+app.get("/jidol", (req, res) => {
+    db.serialize( () => {
+      db.all("select id, name, birthday, birthplace, team from jidol inner join team on jidol.team_id=team.team_id;", (error, row) => {
+        if(error){
+          res.render('show2', {mes:"エラーです"});
+        }
+        res.render('select3', {data:row});
+      })
+    })
+})
+
+
+app.use(function(req, res, next) {
+  res.status(404).send('ページが見つかりません');
+});
+
+app.listen(8080, () => console.log("Example app listening on port 8080!"));
--- insert_all3.js
+++ insert_all3.js
@@ -2,10 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test3.db');
 
 let sqls = [
-  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (3, "櫻井翔", "1982/1/25", "東京都", 1);',
-  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (8, "村上信五", "1982/1/26", "大阪府", 2);',
-  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (16, "藤原丈一郎", "1996/2/8", "大阪府", 3);',
-  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (22, "平野紫耀", "1997/1/29", "愛知県", 4);'
+  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (2, "大野智", "1980/11/26", "東京都", 1);',
+  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (4, "二宮和也", "1983/6/17", "東京都", 1);',
+  'insert into jidol ("id", "name", "birthday", "birthplace", "team_id") values (5, "松本潤", "1983/8/30", "東京都", 1);',
+  
 ]
 
 for(let sql of sqls){
