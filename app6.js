ce184ca64ab00444fb16a0b55bb75307024f69bc
sisisa <105726977+sisisa@users.noreply.github.com>
2023-06-21 16:43:12 +0900

    Update app6.js
    編集:2023年6月21日

--- app6.js
+++ app6.js
@@ -7,6 +7,42 @@ const db = new sqlite3.Database('test.db');
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/db/:id", (req, res) => {
+ db.serialize( () => {
+ db.all("select id, 都道府県, 人口, 大学 from example where id=" + req.params.id + ";", (error, row) => {
+   if( error ) {
+ res.render('show', {mes:"エラーです"});
+   }
+   res.render('db', {data:row});
+   })
+   })
+  })
+
+app.get("/db/:都道府県", (req, res) => {
+ db.serialize( () => {
+ db.all("select id, 都道府県, 人口, 大学 from example where id=" + req.params.id + ";", (error, row) => {
+   if( error ) {
+ res.render('show', {mes:"エラーです"});
+   }
+   res.render('db', {data:row});
+   })
+   })
+  })
+
+app.get("/db/:人口", (req, res) => {
+ db.serialize( () => {
+ db.all("select id, 都道府県, 人口, 大学 from example where id=" + req.params.id + ";", (error, row) => {
+   if( error ) {
+ res.render('show', {mes:"エラーです"});
+   }
+   res.render('db', {data:row});
+   })
+   })
+  })
 app.get("/", (req, res) => {
   const message = "Hello world";
   res.render('show', {mes:message});
@@ -38,6 +74,43 @@ app.get("/top", (req, res) => {
         })
     })
 })
+
+app.post("/insert", (req, res) => {
+  let sql = `
+insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+  `
+console.log(sql); 
+  db.serialize( () => {
+db.run( sql, (error, row) => {
+  console.log(error);
+if(error) {
+res.render('show', {mes:"エラーです"});
+  }
+  // res.redirect('/db');
+
+  });
+    });
+  console.log(req.body);
+  });
+
+app.post("/inpro.html", (req, res) => {
+  let sql = `
+insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+  `
+console.log(sql); 
+  db.serialize( () => {
+db.run( sql, (error, row) => {
+  console.log(error);
+if(error) {
+res.render('show', {mes:"エラーです"});
+  }
+  // res.redirect('/db');
+
+  });
+    });
+  console.log(req.body);
+  });
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
